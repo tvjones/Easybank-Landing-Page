f995eed0ee106f5d766c26d60b2080d3ba6d6d6f
tvjones <43976584+tvjones@users.noreply.github.com>
2021-08-02 18:14:51 -0500

    Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,16 @@
-const loadMobileMenu = () =>{
-    const hamburgerMenu = (document.getElementsByClassName("hamburger-menu"))[0];
-    hamburgerMenu.style.display = 'none';
-    const closeButton = document.createElement('img');
-    closeButton.src = '/images/icon-close.svg';
-    closeButton.className = 'close-button';
-    closeButton.style.cursor = 'pointer';
-    closeButton.addEventListener('click',hideMobileMenu = () =>{
-        closeButton.style.display = 'none';
-        hamburgerMenu.style.display = 'flex';
-        (((document.getElementById('nav-bar')).children[1]).children[0]).style.display='none';
-        
-    });
-    (document.getElementById('nav-bar')).appendChild(closeButton);
-    (((document.getElementById('nav-bar')).children[1]).children[0]).style.display='block';
-   
-}
+const hamburgerIcon = document.getElementsByClassName("hamburger-menu")[0];
+const closeIcon = document.getElementsByClassName("close-icon")[0];
+const navOptions = document.getElementById("nav-bar").children[1].children[0];
+
+// Hamburger Menu
+closeIcon.addEventListener('click', () =>{
+    navOptions.classList.toggle('mobile');
+    hamburgerIcon.classList.toggle('show-hamburger-icon');
+    closeIcon.classList.toggle('show-close-icon');
+});
+
+hamburgerIcon.addEventListener('click', ()=>{
+    navOptions.classList.toggle('mobile');
+    hamburgerIcon.classList.toggle('show-hamburger-icon');
+    closeIcon.classList.toggle('show-close-icon');
+});
